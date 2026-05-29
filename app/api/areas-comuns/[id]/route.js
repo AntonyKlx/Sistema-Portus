b803f0f3155d17ad37b546bb28cb70eb7a5a68c3
gabriel <gabriellegalizado9@gmail.com>
2026-05-29 20:48:43 -0300

    feat: Adiciona função DELETE para gerenciamento de áreas comuns

---
+++ app/api/areas-comuns/[id]/route.js
@@ -0,0 +1,45 @@
+import { NextResponse } from 'next/server'
+import { prisma } from '@/lib/prisma'
+
+export async function DELETE(request, { params }) {
+  try {
+    const id = Number(params.id)
+    //validar id pra não dar cagada
+    if (!Number.isInteger(id) || id <= 0) {
+      return NextResponse.json({ error: 'ID inválido' }, { status: 400 })
+    }
+
+    const existeAreaComum = await prisma.areaComum.findUnique({
+      where: { id: id }
+    })
+    if (!existeAreaComum) return NextResponse.json({ error: 'Área comum não existe' }, { status: 404 })
+
+    const existeReservaAtiva = await prisma.reserva.findFirst({
+      where: {
+        areaComumId: id,
+        status: { in: ['Pendente', 'Aprovada'] } // tomar cuidado aqui por que pode mudar de acordo com o que a gente definir mais pra frente
+      }
+    })
+
+    const url = new URL(request.url);
+    const confirmou = url.searchParams.get('confirmar') === 'true';
+
+    if (existeReservaAtiva && !confirmou) return NextResponse.json({ error: 'Existem reservas pendentes para essa área comum. Confirma a exclusão?' }, { status: 409 })
+
+    await prisma.reserva.deleteMany({
+      where: { areaComumId: id }
+    })
+
+    await prisma.regrasReserva.deleteMany({
+      where: { areaComumId: id }
+    })
+
+    await prisma.areaComum.delete({
+      where: { id: id }
+    })
+
+    return NextResponse.json({ message: 'Área comum deletada com sucesso' }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error: 'Erro ao deletar a área comum' }, { status: 500 })
+  }
+}
